8f8f049aa9ff8f035d04df5f4fd6bf2436c18ae6
Darren <kinwah.lai@gmail.com>
2014-11-25 09:53:16 +0800

    add spock test template

---
+++ src/test/groovy/com/adaptionsoft/games/uglytrivia/GameTest.groovy
@@ -0,0 +1,13 @@
+package com.adaptionsoft.games.uglytrivia
+
+import spock.lang.Specification
+
+/**
+ * Created by darrenlai on 25/11/14.
+ */
+class GameTest extends Specification {
+    def "true is true"() {
+        expect:
+        false
+    }
+}
